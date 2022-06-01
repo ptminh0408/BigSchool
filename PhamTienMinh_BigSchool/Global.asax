8416ec2347b5d9aabada50883cae6ad3de71968b
TienMinh <minhphm23@gmail.com>
2022-06-01 20:43:52 +0700

    first commit

---
+++ PhamTienMinh_BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhamTienMinh_BigSchool.MvcApplication" Language="C#" %>
---
+++ PhamTienMinh_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhamTienMinh_BigSchool.Startup))]
+namespace PhamTienMinh_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
