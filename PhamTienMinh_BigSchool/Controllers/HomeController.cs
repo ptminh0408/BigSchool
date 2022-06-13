badc8b3a67c0ec5aee37b8dbd5d203c45dc60579
TienMinh <minhphm23@gmail.com>
2022-06-13 13:19:09 +0700

    trang 52

--- PhamTienMinh_BigSchool/Controllers/HomeController.cs
+++ PhamTienMinh_BigSchool/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
-﻿using System;
+﻿using PhamTienMinh_BigSchool.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace PhamTienMinh_BigSchool.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext dbContext;
+        public HomeController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcommingCoures = dbContext.Courses
+                .Include(s => s.Lecturer)
+                .Include(s => s.Category)
+                .Where(s => s.DateTime > DateTime.Now);
+            return View(upcommingCoures);
         }
 
         public ActionResult About()
